9ac47250d66656f9c3295d7dcd14d872ec6a1ea8
l <l@liv.io>
2020-12-26 15:51:08 +0100

    Improve info command structure

--- cmd/info.go
+++ cmd/info.go
@@ -24,25 +24,25 @@ The 'info' command shows information about the system.
 		fmt.Println("\n" + "Hostname		" + hostname.GetHostname("short"))
 		fmt.Println("FQDN			" + hostname.GetHostname("long") + "\n")
 
-		fmt.Println("IP Address		" + "10.1.1.11")
-		fmt.Println("Network			" + "10.1.1.0/24")
-		fmt.Println("Default Gateway		" + "10.1.1.1" + "\n")
+		fmt.Println("IP Address" + "		" + "10.1.1.11")
+		fmt.Println("Network" + "			" + "10.1.1.0/24")
+		fmt.Println("Default Gateway" + "		" + "10.1.1.1" + "\n")
 
-		fmt.Println("System Start Time	" + sys.GetBoottime())
-		fmt.Println("System Uptime		" + sys.GetUptime() + "\n")
+		fmt.Println("System Start Time" + "	" + sys.GetBoottime())
+		fmt.Println("System Uptime" + "		" + sys.GetUptime() + "\n")
 
-		fmt.Println("OS Name			" + os.GetName())
-		fmt.Println("OS Version		" + os.GetVersion())
-		fmt.Println("OS Architecture		" + os.GetArchitecture() + "\n")
+		fmt.Println("OS Name" + "			" + os.GetName())
+		fmt.Println("OS Version" + "		" + os.GetVersion())
+		fmt.Println("OS Architecture" + "		" + os.GetArchitecture() + "\n")
 
-		fmt.Println("Processor Sockets	" + cpu.GetSockets())
-		fmt.Println("Processor Cores		" + cpu.GetCores() + "\n")
+		fmt.Println("Processor Sockets" + "	" + cpu.GetSockets())
+		fmt.Println("Processor Cores" + "		" + cpu.GetCores() + "\n")
 
-		fmt.Println("Memory Size		" + mem.GetMemory())
-		fmt.Println("Swap Size		" + mem.GetSwap() + "\n")
+		fmt.Println("Memory Size" + "		" + mem.GetMemory())
+		fmt.Println("Swap Size" + "		" + mem.GetSwap() + "\n")
 
-		fmt.Println("Coremon Start Time	" + coremon.GetStarttime())
-		fmt.Println("Coremon Uptime		" + coremon.GetUptime() + "\n")
+		fmt.Println("Coremon Start Time" + "	" + coremon.GetStarttime())
+		fmt.Println("Coremon Uptime" + "		" + coremon.GetUptime() + "\n")
 	},
 }
 
