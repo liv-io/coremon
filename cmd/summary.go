85728b87cecadaafb6a25297e9e4dec0b6b06b55
l <l@liv.io>
2020-12-26 15:50:31 +0100

    Add basic summary command structure

--- cmd/summary.go
+++ cmd/summary.go
@@ -3,6 +3,9 @@ package cmd
 import (
 	"fmt"
 
+	"github.com/liv-io/coremon/lib/hostname"
+        "github.com/liv-io/coremon/lib/sys"
+
 	"github.com/spf13/cobra"
 )
 
@@ -14,36 +17,33 @@ var summaryCmd = &cobra.Command{
 The 'summary' command shows an overview of the system status and resources.
 `,
 	Run: func(cmd *cobra.Command, args []string) {
-		fmt.Println(`UNIT		TYPE		STATUS
+		fmt.Println("\n" + hostname.GetHostname("short")+ "		" + "host" + "		" + "OK")
+		fmt.Println("Uptime" + "		" + "host" + "		" + sys.GetUptime())
+		fmt.Println("Load" + "		" + "host" + "		" + "0.77, 1.02, 1.15" + "\n")
 
-host01		host		OK
-Uptime		host		11 days 16 hours 38 minutes
-Load		host		0.77, 1.02, 1.15
+		fmt.Println("Processor" + "	" + "system" + "		" + "[||||                                     10.0%]")
+		fmt.Println("  1" + "		" + "system" + "		" + "[|                                         2.7%]")
+		fmt.Println("  2" + "		" + "system" + "		" + "[|                                         1.2%]")
+		fmt.Println("  3" + "		" + "system" + "		" + "[||                                        4.7%]")
+		fmt.Println("  4" + "		" + "system" + "		" + "[|                                         1.4%]")
+		fmt.Println("Memory" + "		" + "system" + "		" + "[|||||||||||||||||||                 2.71G/8.0G]")
+		fmt.Println("Swap" + "		" + "system" + "		" + "[|                                  16.9M/2.00G]")
+		fmt.Println("IOWait" + "		" + "system" + "		" + "[|                                         1.4%]" + "\n")
 
-Processor	system		[||||                                     10.0%]
- 1		system		[|                                         2.7%]
- 2		system		[|                                         1.2%]
- 3		system		[||                                        4.7%]
- 4		system		[|                                         1.4%]
-Memory		system		[|||||||||||||||||||                 2.71G/8.0G]
-Swap		system		[|                                  16.9M/2.00G]
-IOWait		system		[|                                         1.4%]
+		fmt.Println("eth0: in" + "	" + "network" + "		" + "[|                                      9.6Mbps]")
+		fmt.Println("eth0: out" + "	" + "network" + "		" + "[|                                      0.5Mbps]")
+		fmt.Println("lo: in" + "		" + "network" + "		" + "[|                                      0.0Mbps]")
+		fmt.Println("lo: out" + "		" + "network" + "		" + "[|                                      0.0Mbps]" + "\n")
 
-eth0: in	network		[|                                      9.6Mbps]
-eth0: out	network		[                                       0.5Mbps]
-lo: in		network		[                                       0.0Mbps]
-lo: out		network		[                                       0.0Mbps]
+		fmt.Println("sda1" + "		" + "storage" + "		" + "[|||||||                                  17.2%]")
+		fmt.Println("sda2" + "		" + "storage" + "		" + "[||||||||||||                             26.1%]")
+		fmt.Println("sdb1" + "		" + "storage" + "		" + "[|||||||||||||||||||||||                  50.6%]" + "\n")
 
-sda1		storage		[|||||||                                  17.2%]
-sda2		storage		[||||||||||||                             26.1%]
-sdb1		storage		[|||||||||||||||||||||||                  50.6%]
-
-coremon		service		OK
-openssh		service		OK
-chronyd		service		OK
-openntpd	service		OK
-opensmtpd	service		OK
-`)
+		fmt.Println("coremon" + "		" + "service" + "		" + "OK")
+		fmt.Println("openssh" + "		" + "service" + "		" + "OK")
+		fmt.Println("chronyd" + "		" + "service" + "		" + "OK")
+		fmt.Println("openntpd" + "	" + "service" + "		" + "OK")
+		fmt.Println("opensmtpd" + "	" + "service" + "		" + "OK" + "\n")
 	},
 }
 
